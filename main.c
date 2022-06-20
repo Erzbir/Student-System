cab511696b5d309fdb5e3e07c15eea7a44192bd1
Erzbir <2978086497@qq.com>
2022-06-20 09:19:09 +0800

    Update notes

--- main.c
+++ main.c
@@ -19,16 +19,17 @@
 /*
  * This version is implemented as an array and a DoubleLinkList.
  * There is less user data, so it is directly stored in an array.
- * The student data uses a DoubleLinkList uses the simplest bubble sort.
- * The DoubleLinkList is not circular, the next of the last node points to the NULL and does not point to the first node, except that it is the same as the DoubleCircularLinkList,
+ * The student data use a DoubleLinkList use the simplest bubble sort.
+ * The DoubleLinkList is not circular, and the next of the last node points to the NULL and does not point to the first node, except that it is the same as the DoubleCircularLinkList,
  */
 
-//Please modify the file path which was declared in "define.h" before running. The file name can be customized and can be located in different folders, but must be different files.
-//Please change the PAUSE declared in "define.h" to the command under Windows before running the Windows system
+// Please modify the file path which was declared in "define.h" before running. The file name can be customized and can be located in different folders, but must be different files.
+// Please change the PAUSE declared in "define.h" to the command under Windows before running the Windows system
 
-//Need to add all files to the working directory, preferably CLion, if you use C-free or Dev-C++ or VS, opening "main.c" directly and running it.
+// Need to add all files to the working directory, preferably CLion, if you use C-free or Dev-C++ or VS, opening "main.c" directly and running it.
+// If you use C-free or Dev-C++ or VS, just open "main.c" and run it.
 
-//You cannot operate "modify" and "delete" without permission. If you want to operate, you must obtain permission first.
+// You cannot operate "modify" and "delete" without permission. If you want to operate, you must obtain permission first.
 // The management password is: 123456
 
 
